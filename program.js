4ad1343f9d2deef0e2d716e32522ce3943f2c872
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-07-15 22:13:54 -0400

    add grade book program

---
+++ program.js
@@ -0,0 +1,10 @@
+/**
+ * Created by benjaminhuang on 7/15/17.
+ */
+var book = require("./lib/grades").book;
+
+for(var i = 2; i < process.argv.length; i++){
+    book.addGrade(parseInt(process.argv[i]));
+}
+
+console.log(book.getAverage());
---
+++ tests/gradeTests.js
@@ -0,0 +1,31 @@
+/**
+ * Created by benjaminhuang on 7/15/17.
+ */
+
+var grades = require("../lib/grades");
+var book = grades.book;
+
+exports["setUp"] = function (callback) {
+    book.reset();
+    callback();
+
+};
+
+exports["Can add new grade"] = function (test) {
+    book.addGrade(90);
+    var count = book.getCountOfGrades();
+    test.equal(count,1);
+    test.done();
+    
+};
+
+exports["Can average grades"] = function (test) {
+    book.addGrade(100);
+    book.addGrade(50);
+
+    var average = book.getAverage();
+
+    test.equal(average, 75);
+    test.done();
+    
+};
